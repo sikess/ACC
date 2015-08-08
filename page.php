b3e9bff2a3d1129293100594db3502947edd8d71
Roberth <piedrucci@gmail.com>
2015-08-08 18:03:52 -0430

    Pagina

--- page.php
+++ page.php
@@ -6,25 +6,17 @@
 
 get_header(); ?>
 
-<div class="contenedor"><!--CONTENEDOR DE CUERPO Y LISTA DE POST-->
-
-	<div class="entre_seccion"></div><!--F entre_seccion-->
-
-	<div id="conte_cuerpo">
-
-	<div class="conte_todos_post">
-
-
-		<div id="primary">
-			<div id="content-sp">
+<div class="contenedor">
+  <div class="catalogo_cont"><!--Catalogo_cont-->
+    <div class="row">
 
 				<?php the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
 					<header class="entry-header">
-						<h1 class="entry-title"><?php the_title(); ?></h1>
+						<h1 class="entry-title-sp"><?php the_title(); ?></h1>
 					</header><!-- .entry-header -->
-
+					<hr>
 					<div class="entry-content">
 						<?php the_content(); ?>
 						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'themename' ), 'after' => '</div>' ) ); ?>
@@ -39,26 +31,16 @@ get_header(); ?>
 <?php //if (function_exists('wp_corenavi')) wp_corenavi(); ?>
 
 
-	<?php relacionados('Noticias relacionadas'); ?>
+	<?php prod_relacionados(""); ?>
 
-</div>
-
-<div class="el_sidebar">
-
-		<!-- <div class="conte_sidebar"> -->
-			<?php get_sidebar(); ?>
-		<!-- </div> -->
+	</div>
 
 </div>  
 
 </div>  
-
-</div>  </div> 
-
-<div class="entre_seccion"></div><!--F entre_seccion-->
-
 
 <div class="contenedor"><!-- Footer -->
 
 	<?php get_footer();  ?>
 </div><!-- .Footer -->
+
